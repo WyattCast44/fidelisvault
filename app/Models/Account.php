725b8ff64de0136210bbb8a6e460ff98c6899445
Wyatt <wyatt.castaneda@gmail.com>
2018-09-27 19:45:51 -0600

    set the table name for accounts model

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'accounts';
+
     public function user()
     {
         return $this->belongsTo('App\Models\User');
