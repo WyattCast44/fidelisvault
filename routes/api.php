a05c001248449b07ab699c10834ce863ddcc3dda
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-08-25 18:01:57 -0600

    wip

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::middleware('auth')->group(function () {
-
     Route::post('/users/get-account-password', 'AccountsController@apiGetAccountPassword')->name('api.user.get-account-password');
-
 });
